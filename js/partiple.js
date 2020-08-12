55991f349dff13b1978082d69fce9f609e92823c
aneesh0825 <67349950+aneesh0825@users.noreply.github.com>
2020-08-12 18:51:11 -0400

    Add files via upload

---
+++ js/partiple.js
@@ -0,0 +1,26 @@
+class Partiple {
+    constructor(x,y) {
+        var options = {
+            restitution:0.4
+            
+        }
+        this.r = 10
+        this.body = Bodies.circle(x,y,this.r,options);
+        this.color= color(random(0,255),random(0,255),random(0,255));
+        World.add(world,this.body);
+
+
+    }
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push ()
+        translate (pos.x,pos.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        noStroke();
+        fill (this.color);
+        ellipse(0,0,this.r,this.r);
+        pop ();
+    }
+}
